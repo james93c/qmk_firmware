ecc06eb1e6dc9e35b682759d551cd687d0bd2c17
James Cotter <james93cotter@gmail.com>
2024-07-03 23:13:02 +0100

    func/win key on space and enter

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/jc_dvorak/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/jc_dvorak/keymap.c
@@ -16,16 +16,19 @@
 #define CC_LBRA LSFT(KC_LBRC)
 #define CC_RBRA LSFT(KC_RBRC)
 
+#define FNC_SPC LT(FNC, KC_SPC)
+#define GUI_ENT MT(MOD_LGUI, KC_ENT)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[DVORAK] = LAYOUT_5x6(
         KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,                           KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_F12,
         KC_TAB, KC_QUOT,KC_COMM,KC_DOT, KC_P,   KC_Y,                           KC_F,   KC_G,   KC_C,   KC_R,   KC_L,   KC_SLSH,
         KC_ESC, KC_A,   KC_O,   KC_E,   KC_U,   KC_I,                           KC_D,   KC_H,   KC_T,   KC_N,   KC_S,   KC_MINS,
-        KC_LSFT,KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,                           KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,   KC_LSFT,
+        KC_LSFT,KC_SCLN,KC_Q,   KC_J,   KC_K,   KC_X,                           KC_B,   KC_M,   KC_W,   KC_V,   KC_Z,   KC_RSFT,
                         KC_LEFT,KC_RGHT,                                                        KC_UP,  KC_DOWN,
-                                        KC_BSPC,KC_SPC,                         KC_ENT, KC_DEL,
-                                                KC_EQL, KC_LCTL,        KC_LGUI,KC_LBRC,
-                                                KC_BSLS,MO(FNC),        KC_LALT,KC_RBRC
+                                        KC_BSPC,FNC_SPC,                        GUI_ENT, KC_RSFT,
+                                                KC_DEL, KC_LCTL,        KC_TAB, KC_LALT,
+                                                KC_BSLS,KC_EQL,         KC_LBRC,KC_RBRC
                                         // MO(FNC),KC_SPC,                         KC_ENT, KC_DEL,
                                         //         KC_BSPC,KC_LCTL,        KC_LGUI,KC_LBRC,
                                         //         KC_DEL, KC_ENT,         KC_LALT,KC_RBRC
@@ -41,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 _______,_______,        TG(QTY),KC_DOT
     ),
 	[FUNCTION] = LAYOUT_5x6(
-        KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,                          KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
+        KC_F12, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                          KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,
         _______,KC_PGUP,KC_HOME,KC_UP,  KC_END, _______,                        _______,CC_PLUS,CC_PIPE,CC_LBRA,CC_RBRA,_______,
         _______,KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,KC_ENT,                         _______,KC_EQL, KC_BSLS,KC_LBRC,KC_RBRC,_______,
         _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
@@ -58,8 +61,3 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 };
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
-
-
-
-
-
